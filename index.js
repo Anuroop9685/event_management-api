313bc73cda39bc9c660cea3c4b3b82f4829ec18e
Anuroop Jayakumar <anuroopjayakumar73@gmail.com>
2025-05-06 11:39:37 +0530

    Connected to MongoBD

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const PORT= process.env.PORT || 5000
 
 
 app.listen(PORT,() =>{
-    console.log(`Server running on PORT ${PO}`)
+    console.log(`Server running on PORT ${PORT}`)
 })
+
+// Adding mongodb connection
+MONGO_URI 
+
+const mongoose= require('mongoose')
+
+mongoose.connect(process.env.MONGO_URI)
