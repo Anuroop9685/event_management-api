e033350a517ff9b86dd354a520e2f41e7665ffed
Anuroop Jayakumar <anuroopjayakumar73@gmail.com>
2025-05-06 13:36:25 +0530

    Code corrected and updated

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-const express = require('express')
-const app=express() 
 require('dotenv').config()
 
+
+const express = require('express')     // Imported necessary modules
+const app = express()
+const cors = require('cors');
+
+
+const mongoose = require('mongoose')   // Mongoose for MongoDB interaction
+
+app.use(cors()); 
 app.use(express.json())
 
-app.get('/',(res,req) => {
+app.get('/', (req, res) => {           // Fixed parameter order
     res.send('Event Management API is running....')
 })
 
-const PORT= process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
+// Connect to MongoDB using mongoose
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error(err))
 
-app.listen(PORT,() =>{
+// Calling routes
+const eventRoutes = require('./routes/event')
+app.use('/events', eventRoutes)
+
+app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`)
-})
-
-// Adding mongodb connection
-MONGO_URI 
-
-const mongoose= require('mongoose')
-
-mongoose.connect(process.env.MONGO_URI)
+});
